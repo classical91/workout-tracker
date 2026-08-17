df2c4cd9a649f4d284bfdbc12a494ef1f7ce2191
classical91 <jasonandrewasiedu@gmail.com>
2026-08-16 23:53:33 -0700

    Load responsive app shell styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import WorkoutTracker from "./App.jsx";
+import "./app.css";
+
 createRoot(document.getElementById("root")).render(<WorkoutTracker />);
